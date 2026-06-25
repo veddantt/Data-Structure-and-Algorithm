4a84e002ce1e2c58a068d869964af3f2f084cd57
Vedant <148513054+veddantt@users.noreply.github.com>
2026-06-25 09:58:25 -0400

    Time: 0 ms (100%), Space: 12.3 MB (96.2%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def rightSideView(self, root):
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right):]
