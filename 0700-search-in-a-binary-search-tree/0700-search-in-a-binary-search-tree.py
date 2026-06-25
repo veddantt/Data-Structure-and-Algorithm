b253cae63e3d1f838eaa318f377ae62be55a1432
Vedant <148513054+veddantt@users.noreply.github.com>
2026-06-25 10:00:10 -0400

    Time: 0 ms (100%), Space: 16.4 MB (34.82%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def searchBST(self, root, val):
+        if not root or root.val == val:
+            return root
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+        
